fc2ecab3ec1896fb1a66549c34ceb4d717a54428
Denys Potapov <denys.potapov@gmail.com>
2018-10-20 16:45:00 +0300

    add login screen

--- app/shop/models.py
+++ app/shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Proposal(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField('date of sale')
+    usage = models.TextField()
+
+
+class Choice(models.Model):
+    proposal = models.ForeignKey(Proposal, on_delete=models.CASCADE)
+    comment = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
--- app/shop/urls.py
+++ app/shop/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', views.index, name='index'),
 ]
