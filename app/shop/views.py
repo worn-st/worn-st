24b6900a8aff7a797add13411bd26b189aeb19ac
Denys Potapov <denys.potapov@gmail.com>
2018-10-20 18:14:37 +0300

    add future sales

--- app/shop/views.py
+++ app/shop/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Future
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+@login_required
 def futures(request):
     return render(request, 'futures.html', {
+    	'futures': Future.objects.filter(user=request.user)
     })
